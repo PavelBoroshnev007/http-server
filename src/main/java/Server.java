673969b2bf347e2185992384deedf86ab2162511
Pavel Boroshnev <pavelboroshnev9@gmail.com>
2023-10-11 17:47:09 +0300

    homework revision2

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.Socket;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Server {
     private final Socket socket;
@@ -14,8 +15,39 @@ public class Server {
                 final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 final var out = new BufferedOutputStream(socket.getOutputStream());
         ) {
-            // Здесь будет ваша обработка подключения
-            // ...
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                return;
+            }
+
+            final var path = parts[1];
+            final var filePath = Path.of(".", "public", path);
+
+            if (!Files.exists(filePath) || !Files.isRegularFile(filePath)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+            }
+
+            final var mimeType = Files.probeContentType(filePath);
+            final var length = Files.size(filePath);
+
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
